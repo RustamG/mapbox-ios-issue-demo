8c333836ee4c865b7417883f7acffb79ec308a49
rustamg <ryder.norco@gmail.com>
2020-06-11 16:26:49 +0400

    Initial Commit

---
+++ mapbox-issue-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mapbox-issue-demo
+//
+//  Created by Rustam G on 11.06.2020.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
